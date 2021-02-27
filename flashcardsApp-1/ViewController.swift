cbcb3a805bf01a132a0ccf013bdbf5bd89429427
khadillacs <khadijahakter123@gmail.com>
2021-02-27 01:19:14 -0500

    Initial Commit

---
+++ flashcardsApp-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashcardsApp-1
+//
+//  Created by Khadijah Akter on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
